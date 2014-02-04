db09ee93d07b137a8670ca3a5a5905874e427370
Jamieson Abbott <jdabbott@vt.edu>
2014-02-03 20:17:35 -0500

    Initial Scaffolding

---
+++ InnerFear/Classes/AppDelegate.h
@@ -0,0 +1,10 @@
+//
+//  AppDelegate.h
+//  AppInnerFear
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@end
---
+++ InnerFear/Classes/Game.h
@@ -0,0 +1,11 @@
+//
+//  Game.h
+//  AppInnerFear
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIDevice.h>
+
+@interface Game : SPSprite
+
+@end
